a35a81aa0903e02e61fc717f2d64a667b0277ea5
Philip Housden <philip.houden.04@gmail.com>
2024-06-26 19:41:57 +0700

    implementing buying button

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ createApp ({
         const image = ref('./assets/images/socks_green.jpg')
         const camt = ref('https://www.camt.cmu.ac.th/index.php/th/')
         const inStock = ref(true)
-        const inventory = ref(100)
+        const inventory = ref(12)
         const onSale = ref(true)
         const details = ref([
             '50% cotton',
@@ -15,14 +15,26 @@ createApp ({
             '20% polyester'
         ])
         const variants = ref([
-            {id: 2234, color: 'green'},
-            {id: 2235, color: 'blue'}
+            {id: 2234, color: 'green', image:'./assets/images/socks_green.jpg'},
+            {id: 2235, color: 'blue', image:'./assets/images/socks_blue.jpg'}
         ])
         const sizes = ref([
             'S',
             'M',
             'L'
         ])
+        const cart = ref(0)
+
+        function addToCart() {
+            cart.value += 1
+        }
+        function updateImage(variantImage){
+            image.value = variantImage
+        }
+        function buyBtn() {
+            inventory.value -= 1;
+            console.log(inventory)
+        }
 
 
         return {
@@ -35,7 +47,11 @@ createApp ({
             onSale,
             details,
             variants,
-            sizes
+            sizes,
+            cart,
+            addToCart,
+            updateImage,
+            buyBtn
         }
     }
 }).mount('#app')
