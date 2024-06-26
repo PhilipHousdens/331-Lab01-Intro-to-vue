0aced2fbbeb039c325954320c21258f841971e9b
Philip Housden <philip.houden.04@gmail.com>
2024-06-26 19:14:17 +0700

    using v-bind for camt url

--- main.js
+++ main.js
@@ -4,11 +4,13 @@ createApp ({
     setup() {
         const product = ref('Boots')
         const image = ref('./assets/images/socks_green.jpg')
+        const camt = ref('https://www.camt.cmu.ac.th/index.php/th/')
         const inStock = ref(true)
         const inventory = ref(100)
         return {
             product,
             image,
+            camt,
             inStock,
             inventory
         }
