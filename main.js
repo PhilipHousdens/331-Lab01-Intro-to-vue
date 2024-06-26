c20d25dbe7706ec760061ae394f217baf1bf646f
Philip Housden <philip.houden.04@gmail.com>
2024-06-26 19:17:56 +0700

    adding discription and sales

--- main.js
+++ main.js
@@ -2,17 +2,21 @@ const { createApp, ref } = Vue;
 
 createApp ({
     setup() {
-        const product = ref('Boots')
+        const product = ref('Socks')
+        const discription = ref('Cotton Socks')
         const image = ref('./assets/images/socks_green.jpg')
         const camt = ref('https://www.camt.cmu.ac.th/index.php/th/')
         const inStock = ref(true)
         const inventory = ref(100)
+        const onSale = ref(true)
         return {
             product,
+            discription,
             image,
             camt,
             inStock,
-            inventory
+            inventory,
+            onSale
         }
     }
 }).mount('#app')
